548dda102f33b79caacf59bf0015096960e8c613
prasad <prasaddevops@gmail.com>
2022-06-07 14:04:47 +0530

    spring boot code

---
+++ src/main/java/net/javaguides/sprinbboot/SprinbbootDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.sprinbboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbbootDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbbootDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/sprinbboot/SprinbbootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.sprinbboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbbootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
